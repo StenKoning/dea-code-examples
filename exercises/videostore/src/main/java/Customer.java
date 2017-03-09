36c98fee395e5dace9456edf3cf21e14734af972
AS Koning <stenkoning+gitkraken@gmail.com>
2017-03-09 11:50:50 +0100

    Gemaakt

--- exercises/videostore/src/main/java/Customer.java
+++ exercises/videostore/src/main/java/Customer.java
@@ -13,28 +13,61 @@ public class Customer {
 	public String getName() {
 		return _name;
 	}
+
 	public String statement() {
-		double totalAmount = 0;
-		int frequentRenterPoints = 0;
 		Enumeration rentals = _rentals.elements();
 		String result = "Rental Record for " + getName() +  "\n";
+
 		while (rentals.hasMoreElements()) {
 			Rental each = (Rental) rentals.nextElement();
-			// add frequent renter points
-			frequentRenterPoints ++;
-			// add bonus for a two day new release rental
-			if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
-			    each.getDaysRented() > 1) frequentRenterPoints++;
 			//show figures for this rental
 			result += "\t" + each.getMovie().getTitle()+ "\t" +
 				String.valueOf(each.getCharge()) + "\n";
-			totalAmount += each.getCharge();
 		}
-		// add footer lines
-		result += "Amount owed is "+String.valueOf(totalAmount) + "\n";
-		result += "You earned "+String.valueOf(frequentRenterPoints) 
+
+		// add footer lines®
+		result += "Amount owed is "+String.valueOf(getTotalCharge()) + "\n";
+		result += "You earned "+String.valueOf(getTotalFrequentRenterPoints())
  					+ " frequent renter points\n";
 		return result;
 	}
 
+
+	private double getTotalCharge(){
+		double totalAmount = 0;
+		Enumeration rentals = _rentals.elements();
+		while (rentals.hasMoreElements()) {
+			Rental each = (Rental) rentals.nextElement();
+			totalAmount += each.getCharge();
+		}
+		return totalAmount;
+	}
+
+	private int getTotalFrequentRenterPoints(){
+		int frequentRenterPoints = 0;
+		Enumeration rentals = _rentals.elements();
+		while (rentals.hasMoreElements()) {
+			Rental each = (Rental) rentals.nextElement();
+			frequentRenterPoints += each.getFrequentRenterPoints();
+		}
+		return frequentRenterPoints;
+	}
+
+	public String htmlStatement() {
+		Enumeration rentals = _rentals.elements();
+		String result = "<H1>Rental Record for<EM> " +
+				getName() +  "<EM></H1><P>\n";
+		while (rentals.hasMoreElements()) {
+			Rental each = (Rental) rentals.nextElement();
+			//show figures for this rental
+			result +=  each.getMovie().getTitle()+ ": " +
+					String.valueOf(each.getCharge()) + "<BR>\n";
+		}
+		// add footer lines
+		result += "<P>Amount owed is<EM> "+String.valueOf(getTotalCharge())
+				+ "</EM><P>\n";
+		result += "You earned <EM>"+String.valueOf(getTotalFrequentRenterPoints())+
+				"</EM> frequent renter points<P>\n";
+		return result;
+	}
 }
